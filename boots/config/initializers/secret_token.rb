8c1999e8dddd57c16054a27e55b8e05c36720101
firminsa <firminsa@gmail.com>
2016-02-29 12:26:42 -0500

    forms project

---
+++ boots/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Boots::Application.config.secret_key_base = '228695ffa7773be114b60cd15e473604a61f6bad5256d688ca1157fab53014978df68fd72f668148b6e2d01a72539c599de05c6253ff156421cab987ad30795c'
